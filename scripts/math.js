2fe7f943c72751ec20af92c63c825bdd751e839f
jolson615 <jolson615@gmail.com>
2017-08-20 11:32:27 -0400

    initial commit

---
+++ scripts/math.js
@@ -0,0 +1,32 @@
+console.log("JS successfully loaded")
+
+document.addEventListener('DOMContentLoaded', function(){
+  var calculateButton = document.getElementById("calculateButton")
+
+  calculateButton.addEventListener("click", function(){
+    console.log("button pressed")
+    var grade = parseFloat(document.getElementById("grade").value)
+    console.log(grade)
+    var aMin = grade - 5
+    var aMax = grade + 5
+    var bMin = grade - 16
+    var bMax = grade - 6
+    // Look up JS ternary statements later
+    document.getElementById("aMin").innerHTML = aMin
+    document.getElementById("aMax").innerHTML = aMax
+    document.getElementById("bMin").innerHTML = bMin
+    document.getElementById("bMax").innerHTML = bMax
+  })
+})
+
+
+
+
+
+// Write an event listener for the button.
+
+// Run calculations based on user data.
+
+// v.1 Return those calculations as a string with a series of breaks between lines.
+
+// v.2 Return those calculations as a table.
