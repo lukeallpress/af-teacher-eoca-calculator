bb90796927a89325d336435c1ab9e71657b83420
lukeallpress <lukeallpress@gmail.com>
2017-09-07 21:55:15 -0700

    Formulas and all letters

--- scripts/math.js
+++ scripts/math.js
@@ -10,15 +10,27 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log("button pressed")
     var grade = parseFloat(document.getElementById("grade").value)
     console.log(grade)
-    var aMin = grade - 5
-    var aMax = grade + 5
-    var bMin = grade - 16
-    var bMax = grade - 6
+    var aMin = (90 - grade*.8)*5
+    var aMax = (100-grade*.8)*5
+    var bMin = (80 - grade*.8)*5
+    var bMax = (90 - grade*.8)*5
+    var cMin = (70 - grade*.8)*5
+    var cMax = (80 - grade*.8)*5
+    var dMin = (60 - grade*.8)*5
+    var dMax = (70 - grade*.8)*5
+    var fMin = (0 - grade*.8)*5
+    var fMax = (70 - grade*.8)*5
     // Look up JS ternary statements later
     document.getElementById("aMin").innerHTML = aMin
     document.getElementById("aMax").innerHTML = aMax
     document.getElementById("bMin").innerHTML = bMin
     document.getElementById("bMax").innerHTML = bMax
+    document.getElementById("cMin").innerHTML = cMin
+    document.getElementById("cMax").innerHTML = cMax
+    document.getElementById("dMin").innerHTML = dMin
+    document.getElementById("dMax").innerHTML = dMax
+    document.getElementById("fMin").innerHTML = fMin
+    document.getElementById("fMax").innerHTML = fMax
     // show the table
     resultsTable.style.visibility = "visible"
   })
